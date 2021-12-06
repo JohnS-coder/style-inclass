62d46ef581dac83a902936cc2fad85421b0c5330
john_s <john.s@clarusway.com>
2021-12-06 23:06:38 +0300

    sass and css module added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Inline from "./components/imline/inline"
 import StyleSheet from "./components/regular-style/StyleSheet"
+import Sassy from "./components/sassy/Sassy";
+import ModuleBtn from "./components/moduleCss/moduleBtn";
 
 function App() {
   return (
     <div >
       {/* <h1>Clarusway</h1> */}
       {/* <Inline/> */}
-      <StyleSheet/>
+      {/* <StyleSheet btnClass= {false} btnName= "Module" /> */}
+      <Sassy/>
+      {/* <ModuleBtn btnClass="primary-a" btnName="btn"/> */}
     </div>
   );
 }
--- src/components/regular-style/StyleSheet.js
+++ src/components/regular-style/StyleSheet.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import "./StyleSheet.css"
 
-const StyleSheet = () => {
+const StyleSheet = (props) => {
+    const isPrimary = props.btnClass ? "primary" : "secondary"
     return (
         <div>
             <img className="logo" src="https://secure.meetupstatic.com/photos/event/3/1/b/9/600_488352729.jpeg" alt="clarusway-logo" />
             <h1 className="header">Welcome to Clarusway</h1>
-            <p className="primary">Change your skillset, change your future!</p>
+            <p className={isPrimary}>{props.btnName}</p>
         </div>
     )
 }
