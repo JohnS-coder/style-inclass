d7f766feb7b7d7e338018c0d606b2d0ed5dfa501
john_s <john.s@clarusway.com>
2021-12-06 20:47:01 +0300

    inline styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Inline from "./components/imline/inline"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      {/* <h1>Clarusway</h1> */}
+      <Inline/>
     </div>
   );
 }
---
+++ src/components/imline/inline.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Inline = () => {
+    return (
+        <div style={{backgroundColor:"red", color:"#fff"}}>
+            <h1 style={{backgroundColor:"yellow", color:"black"}} >Hello from inline component</h1>
+            <p>Inline styling in react</p>
+        </div>
+    )
+}
+
+export default Inline
